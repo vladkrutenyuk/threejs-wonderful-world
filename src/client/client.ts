518489a91d655be60667d78da1c5d99886284291
KVY <kvy@phygitalism.com>
2020-10-20 11:50:20 +0300

    visual & interactive improvements

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,14 +3,14 @@ import { OrbitControls } from '/jsm/controls/OrbitControls'
 import Stats from '/jsm/libs/stats.module'
 import { GUI } from '/jsm/libs/dat.gui.module'
 
-import { Vector3, Scene, Color, PerspectiveCamera, WebGLRenderer, Mesh, AxesHelper, Material, IcosahedronGeometry, MeshBasicMaterial, Texture, TextureLoader, CubeTextureLoader, DirectionalLight, Vector2, PlaneGeometry, Object3D, RingGeometry, Raycaster, PointLight, OrthographicCamera, CylinderGeometry, ConeBufferGeometry, Vector} from '/build/three.module.js'
+import { Vector3, Scene, Color, PerspectiveCamera, WebGLRenderer, Mesh, AxesHelper, Material, IcosahedronGeometry, MeshBasicMaterial, Texture, TextureLoader, CubeTextureLoader, DirectionalLight, Vector2, PlaneGeometry, Object3D, RingGeometry, Raycaster, PointLight, OrthographicCamera, CylinderGeometry, ConeBufferGeometry, Vector, GridHelper, Line, LineBasicMaterial, BufferGeometry} from '/build/three.module.js'
 
 const scene: Scene = new Scene()    
 scene.background = new Color(0x101010)
 
 const camera: PerspectiveCamera = new PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 150)
 
-const renderer: WebGLRenderer = new WebGLRenderer()
+const renderer: WebGLRenderer = new WebGLRenderer( {antialias: true} )
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -42,43 +42,47 @@ var planeMatData = {
     displacementBias: -0.25,
     displacementScale: 0.75,
     wireframe: true,
-    center: new Vector2(.5, .5)
+    center: new Vector2(.5, .5),
+    transparent: true,
+    opacity: 0.6
 } 
 
-const planeMaterial: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial()
-
-planeMaterial.map = planeMatData.map
-planeMaterial.specularMap = planeMatData.specularMap
-planeMaterial.displacementMap = planeMatData.displacementMap
-planeMaterial.displacementBias = planeMatData.displacementBias
-planeMaterial.displacementScale = planeMatData.displacementScale
-planeMaterial.wireframe = planeMatData.wireframe
-planeMaterial.map.center = planeMatData.center
+const planeMaterial: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial(planeMatData)
 
 const planeMesh: Mesh = new Mesh(planeGeometry, planeMaterial)
 scene.add(planeMesh)
 
-camera.position.set(0, -0.5, 2.5)
+let linesOffsetZ = 0
+var lineVertGeom = new BufferGeometry().setFromPoints( [
+    new Vector3(0, planeData.height / 2, linesOffsetZ), 
+    new Vector3(0, -planeData.height / 2, linesOffsetZ) 
+] );
+
+var lineVertical = new Line(lineVertGeom, new LineBasicMaterial({color: 0xffffff}));
+scene.add(lineVertical);
+
+var lineHorizGeom = new BufferGeometry().setFromPoints( [
+    new Vector3(planeData.width / 2, 0, linesOffsetZ), 
+    new Vector3(-planeData.width / 2, 0, linesOffsetZ) 
+] );
+
+var lineHorizontal = new Line(lineHorizGeom, new LineBasicMaterial({color: 0xffffff}));
+scene.add(lineHorizontal);
+
+camera.position.set(0, 0.2, 2)
 camera.lookAt(0, 0, 0)
 
-const stats = Stats()
-document.body.appendChild(stats.dom)
+// const stats = Stats()
+// document.body.appendChild(stats.dom)
 
-const columnsParent = new Mesh()
-scene.add(columnsParent)
+// const columnsParent = new Mesh()
+// scene.add(columnsParent)
 
-var columns : Mesh[] = new Array()
+// var columns : Mesh[] = new Array()
 
 // const gui = new GUI()
 // const folder = gui.addFolder("Columns Parent")
-// var scale = { scale: 1,}
 // folder.add(columnsParent.position, "x", -4, 4, 0.01)
-// folder.add(columnsParent.position, "y", -4, 4, 0.01)
-// folder.add(columnsParent.position, "z", -4, 4, 0.01)
-// folder.add(columnsParent.scale, "x", 1, 10, 0.01)
-// folder.add(columnsParent.scale, "y", 1, 10, 0.01)
-// folder.add(columnsParent.scale, "z", 1, 10, 0.01)
-// folder.open()
 
 function onKeyDown (event) {
     const step : number = 0.015
@@ -118,19 +122,6 @@ function moveMap(step : Vector2) {
 
     planeMaterial.map.center.x += step.x * planeMaterial.map.repeat.x;
     planeMaterial.map.center.y += step.y * planeMaterial.map.repeat.x;
-    
-    columnsParent.position.x = -(planeMaterial.map.center.x - 0.5) / planeMaterial.map.repeat.x
-    columnsParent.position.y = -(planeMaterial.map.center.y - 0.5) / planeMaterial.map.repeat.y
-
-    // for (let i = 0; i < columns.length; i++) {
-    //     var column = columns[i]
-
-    //     column.position.x -= step.x * planeData.width / planeMaterial.map.repeat.x
-    //     column.position.y -= step.y * planeData.height / planeMaterial.map.repeat.x
-
-    //     var isHidden: boolean = Math.abs(column.position.x) > planeData.width / 2 || Math.abs(column.position.y) > planeData.height / 2
-    //     column.visible = !isHidden
-    // }
 }
 
 function scaleMap(step: number) {
@@ -140,20 +131,6 @@ function scaleMap(step: number) {
         return
     }   
     planeMaterial.map.repeat.addScalar(step)
-
-    columnsParent.scale.set(1 / planeMaterial.map.repeat.x, 1 / planeMaterial.map.repeat.y, 1)
-
-    // for (let i = 0; i < columns.length; i++) {
-    //     var column = columns[i]
-
-    //     var divider = 1 / (repeatX - step)
-
-    //     column.position.divideScalar(divider).multiplyScalar(1 / repeatX)
-    //     column.position.z = 0.1
-
-    //     var isHidden: boolean = Math.abs(column.position.x) > planeData.width / 2 || Math.abs(column.position.y) > planeData.height / 2
-    //     column.visible = !isHidden
-    // }
 }
 
 document.addEventListener('keydown', onKeyDown, false);
@@ -164,8 +141,8 @@ const ringSightData = {
     thetaSegments : 16
 }
 
-const ringGeom = new RingGeometry( ringSightData.innerRadius, ringSightData.outerRadius, ringSightData.thetaSegments)
-const ringMat = new MeshBasicMaterial( {color: new Color(0xff0000)} )
+const ringGeom = new RingGeometry(ringSightData.innerRadius, ringSightData.outerRadius, ringSightData.thetaSegments)
+const ringMat = new MeshBasicMaterial( {color: new Color(0xffffff)} )
 const ringSight = new Mesh(ringGeom, ringMat)
 scene.add(ringSight)
 
@@ -182,12 +159,9 @@ function onMouseMove(event: MouseEvent) {
     raycaster.setFromCamera(mousePos, camera)
     var intersect = raycaster.intersectObject(planeMesh);
     ringSight.position.copy(intersect[0].point)
-}
 
-const columnData = {
-    radiusTop : 0.008,
-    radiusBottom : 0.003, 
-    height : 0.4
+    lineHorizontal.position.y = ringSight.position.y
+    lineVertical.position.x = ringSight.position.x
 }
 
 document.addEventListener('dblclick', onDoubleClick, false)
@@ -200,20 +174,6 @@ function onDoubleClick(event: MouseEvent) {
 
     raycaster.setFromCamera(mousePos, camera)
     var intersect = raycaster.intersectObject(planeMesh);
-    
-    const column = new Mesh(
-        new CylinderGeometry(columnData.radiusTop, columnData.radiusBottom, columnData.height),
-        new MeshBasicMaterial( {color: 0xff0000} )
-    )
-
-    column.position.copy(intersect[0].point)
-    column.rotation.x = Math.PI / 2
-    column.position.z += 0.1;
-
-    columns.push(column)
-    scene.add(column)
-    column.parent = columnsParent;
-    console.log("Added new column: " + column.position.x)
 }
 
 window.addEventListener('resize', onWindowResize, false)
