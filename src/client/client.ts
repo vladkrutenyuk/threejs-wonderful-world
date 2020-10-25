e708ecfc74d858305aa8964ff7211b0cf997cc73
KVY <kvy@phygitalism.com>
2020-10-25 18:49:20 +0300

    super markers and etc

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,8 +2,7 @@ import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
 import { TWEEN } from '/jsm/libs/tween.module.min'
 
-import { Vector3, Scene, Color, PerspectiveCamera, WebGLRenderer, Mesh, AxesHelper, Material, IcosahedronGeometry, MeshBasicMaterial, Texture, TextureLoader, CubeTextureLoader, DirectionalLight, Vector2, PlaneGeometry, Object3D, RingGeometry, Raycaster, PointLight, OrthographicCamera, CylinderGeometry, ConeBufferGeometry, Vector, GridHelper, Line, LineBasicMaterial, BufferGeometry, SphereGeometry} from '/build/three.module.js'
-
+import { Group, Vector3, Scene, Color, PerspectiveCamera, WebGLRenderer, Mesh, AxesHelper, Material, IcosahedronGeometry, MeshBasicMaterial, Texture, TextureLoader, CubeTextureLoader, DirectionalLight, Vector2, PlaneGeometry, Object3D, RingGeometry, Raycaster, PointLight, OrthographicCamera, CylinderGeometry, ConeBufferGeometry, Vector, GridHelper, Line, LineBasicMaterial, BufferGeometry, SphereGeometry} from '/build/three.module.js'
 
 const scene: Scene = new Scene()
 scene.background = new Color(0x101010)
@@ -69,10 +68,8 @@ scene.add(lineHorizontal);
 camera.position.set(0, 0.2, 2)
 camera.lookAt(0, 0, 0)
 
-planeMaterial.map.center.set(0.5, 0.5)
-
 function onKeyDown(event) {
-    const step: number = 0.015
+    const step: number = 0.005
 
     switch (event.keyCode) {
         case 87: // w
@@ -94,19 +91,12 @@ function onKeyDown(event) {
             scaleMap(-step)
             break;
     }
-    // const div = document.getElementById('help-container');
-    // div.innerHTML = event.keyCode;
+
 }
 
-const markersParent = new Mesh()
+let markersGroup = new Group();
 
-const marker = new Mesh(
-    new SphereGeometry(0.005),
-    new MeshBasicMaterial({
-        color: new Color(1 ,1 ,1)
-    })
-)
-scene.add(marker)
+scene.add(markersGroup)
 
 function getMapScale() {
     return 1 / planeMaterial.map.repeat.x
@@ -129,26 +119,27 @@ function scaleMap(step: number) {
 
     fixOffsetMap()
 
-    marker.scale.setScalar(getMapScale())
+    markersGroup.scale.setScalar(getMapScale())
 }
 
 function fixOffsetMap() {
     planeMaterial.map.offset.clampScalar(-getOffsetLimit(), getOffsetLimit())
 
-    marker.position.set(
-        -planeMaterial.map.offset.x * planeGeometry.parameters.width * getMapScale(),
-        -planeMaterial.map.offset.y * planeGeometry.parameters.height * getMapScale(),
-        0
-    )
+    markersGroup.position.x = -planeMaterial.map.offset.x * planeGeometry.parameters.width * getMapScale()
+    markersGroup.position.y = -planeMaterial.map.offset.y * planeGeometry.parameters.height * getMapScale()
 }
 
 document.addEventListener('keydown', onKeyDown, false);
 
+let ringSightData = {
+    innerRadius: 0.03,
+    outerRadius: 0.05,
+    thetaSegments: 16
+}
+
 const ringSight = new Mesh(
     new RingGeometry(
-        0.03,
-        0.05,
-        16
+        ringSightData.innerRadius, ringSightData.outerRadius, ringSightData.thetaSegments
     )
     , new MeshBasicMaterial({
         color: new Color(0xffffff),
@@ -158,10 +149,14 @@ const ringSight = new Mesh(
 )
 
 scene.add(ringSight)
-
 const raycaster = new Raycaster()
+let isMouseOverMarker : boolean = false
+let lerpToMarker = { value: 0 }
+let worldPosMarker: Vector3 = new Vector3().setScalar(0)
 
+const divInfo = document.getElementById('info');
 document.addEventListener('mousemove', onMouseMove, false)
+
 
 function onMouseMove(event: MouseEvent) {
     const mousePos = {
@@ -170,14 +165,51 @@ function onMouseMove(event: MouseEvent) {
     }
 
     raycaster.setFromCamera(mousePos, camera)
-    const intersect = raycaster.intersectObject(planeMesh);
-    ringSight.position.copy(intersect[0].point)
 
-    lineHorizontal.position.y = ringSight.position.y
-    lineVertical.position.x = ringSight.position.x
+    const planeIntersect = raycaster.intersectObject(planeMesh);
+    const markerIntersects = raycaster.intersectObjects(markersGroup.children)
 
-    sightLight.position.y = ringSight.position.y
-    sightLight.position.x = ringSight.position.x
+    if (planeIntersect.length > 0) {
+        ringSight.position.copy(planeIntersect[0].point)
+        lineHorizontal.position.y = ringSight.position.y
+        lineVertical.position.x = ringSight.position.x
+        sightLight.position.y = ringSight.position.y
+        sightLight.position.x = ringSight.position.x
+    }
+
+    if (markerIntersects.length > 0) {
+        new TWEEN.Tween(lerpToMarker).to({value: 0.95}, 250).start()
+        markerIntersects[0].object.getWorldPosition(worldPosMarker)
+
+        if (!isMouseOverMarker) {
+            new TWEEN.Tween(ringSight.scale).to(new Vector3().setScalar(1.2), 250).start()
+            regenerateRingSightGeometry(0.045)
+            isMouseOverMarker = true
+            divInfo.innerHTML = "point: " + worldPosMarker.x.toString().slice(0, 5) + " ; " + worldPosMarker.y.toString().slice(0, 5)
+        }
+    }
+    else {
+        if (isMouseOverMarker){
+            new TWEEN.Tween(lerpToMarker).to({value: 0}, 300).start()
+            new TWEEN.Tween(ringSight.scale).to(new Vector3().setScalar(1), 150).start()
+            regenerateRingSightGeometry(0.03)
+            divInfo.innerHTML = "";
+            isMouseOverMarker = false
+        }
+    }
+
+    ringSight.position.lerpVectors(ringSight.position, worldPosMarker, lerpToMarker.value)
+}
+
+function regenerateRingSightGeometry(newInnerRadius: number) {
+    new TWEEN.Tween(ringSightData)
+        .to({innerRadius: newInnerRadius}, 350)
+        .start()
+        .onUpdate(() => {
+            ringSight.geometry.dispose();
+            ringSight.geometry = new THREE.RingGeometry(
+                ringSightData.innerRadius, ringSightData.outerRadius, ringSightData.thetaSegments)
+        })
 }
 
 document.addEventListener('dblclick', onDoubleClick, false)
@@ -190,6 +222,31 @@ function onDoubleClick(event: MouseEvent) {
 
     raycaster.setFromCamera(mousePos, camera)
     const intersect = raycaster.intersectObject(planeMesh)[0];
+
+    const marker = new Mesh(
+        new SphereGeometry(0.08),
+        new MeshBasicMaterial({
+            color: new Color(1 ,1 ,1),
+            visible: false
+        })
+    )
+    const markerVisual = new Mesh(
+        new SphereGeometry(0.02),
+        new MeshBasicMaterial({
+            color: new Color(1 ,1 ,1),
+            transparent: false
+        })
+    )
+    marker.position.copy(intersect.point)
+    marker.scale.setScalar(0)
+    scene.add(marker)
+    scene.add(markerVisual)
+    markerVisual.parent = marker
+    new TWEEN.Tween(marker.scale)
+        .to(new Vector3().setScalar(1), 500)
+        .easing(TWEEN.Easing.Back.Out)
+        .start()
+        .onComplete(() => markersGroup.add(marker))
 
     new TWEEN.Tween(ringSight.scale).to(new Vector3().setScalar(0), 350).easing(TWEEN.Easing.Back.InOut).start().onComplete(resetRing)
     new TWEEN.Tween(ringSight.material).to( {opacity: 0} , 350).easing(TWEEN.Easing.Cubic.InOut).start()
@@ -220,4 +277,4 @@ function render() {
     renderer.render(scene, camera)
 }
 
-animate();
+animate()
