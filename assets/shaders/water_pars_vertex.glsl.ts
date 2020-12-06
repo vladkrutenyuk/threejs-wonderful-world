07101705fa46818f7854daf87f06051cc780aef1
KVY <kvy@phygitalism.com>
2020-12-07 00:04:05 +0300

    add shaders

---
+++ assets/shaders/water_pars_vertex.glsl.ts
@@ -0,0 +1,10 @@
+export default /* glsl */`
+#ifdef USE_DISPLACEMENTMAP
+	uniform sampler2D displacementMap;
+	uniform float displacementScale;
+	uniform float displacementBias;
+	
+	uniform float waterStep;
+	uniform float time;
+#endif
+`;
---
+++ assets/shaders/water_vertex.glsl.ts
@@ -0,0 +1,5 @@
+export default /* glsl */`
+#ifdef USE_DISPLACEMENTMAP
+	transformed += normalize(objectNormal) * (texture2D(displacementMap, vUv).x * displacementScale + displacementBias);
+#endif
+`;
